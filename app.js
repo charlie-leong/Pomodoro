437fa866048d703337397efc0f2974af4435806f
Charlotte Leong <charlotte.leong@kcl.ac.uk>
2024-05-29 17:11:31 +0100

    add functionality to change the text content on page during countdown

--- app.js
+++ app.js
@@ -14,7 +14,27 @@ const appTimer = () => {
         let totalSeconds = sessionAmount * 60;
 
         const updateSeconds = () => {
+            const minuteDiv = document.querySelector('.minutes');
+            const secondDiv = document.querySelector('.seconds');
 
+            totalSeconds--;
+
+            let minuteesLeft = Math.floor(totalSeconds/60);
+            let secondsLeft = totalSeconds % 60;
+
+            if(secondsLeft < 10){
+                secondDiv.textContent = '0' + secondsLeft;
+            }
+            else {
+                secondDiv.textContent = secondsLeft;
+            }
+
+            minuteDiv.textContent = `${minutesLeft}`
+
+            if(minutesLeft === 0 && secondsLeft === 0){
+                bells.play()
+                clearInterval(myInterval);
+            }
         }
 
         myInterval = setInterval(updateSeconds, 1000);
