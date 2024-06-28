903f052785ec05f4d0d14bf4e2f831bf383bebf4
Charlotte Leong <charlotte.leong@kcl.ac.uk>
2024-06-28 11:08:54 +0100

    add loading animation for first half (change colour for clarity)

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const bells = new Audio('./sounds/bell.wav');
 const startBtn = document.querySelector('.btn-start');
 const session = document.querySelector('.minutes');
+const rightSide = document.querySelector('.right-side');
+const leftSide = document.querySelector('.left-side');
 
 let myInterval;
 let state = true; // if the application is running so the timer progresses
@@ -12,12 +14,14 @@ const appTimer = () => {
     if(state){
         state = false;
         let totalSeconds = sessionAmount * 60;
+        let elapsedSeconds = 0;
 
         const updateSeconds = () => {
             const minuteDiv = document.querySelector('.minutes');
             const secondDiv = document.querySelector('.seconds');
 
             totalSeconds--;
+            elapsedSeconds++;
 
             let minutesLeft = Math.floor(totalSeconds/60);
             let secondsLeft = totalSeconds % 60;
@@ -31,12 +35,24 @@ const appTimer = () => {
 
             minuteDiv.textContent = `${minutesLeft}`
 
+            const percentageElapsed = (elapsedSeconds / (sessionAmount * 60)) * 100;
+
+            if (percentageElapsed <= 50) {
+                rightSide.style.transform = `rotate(${-(percentageElapsed / 50) * 180}deg)`;
+                // leftSide.style.transform = 'rotate(0deg)';
+            } else {
+                // rightSide.style.transform = 'rotate(180deg)';
+                leftSide.style.transform = `rotate(${-((percentageElapsed - 50) / 50) * 180}deg)`;
+
+            }
+
             if(minutesLeft === 0 && secondsLeft === 0){
                 bells.play()
                 clearInterval(myInterval);
                 state = true;
+                minuteDiv.textContent = '5';
             }
-        }
+        };
 
         myInterval = setInterval(updateSeconds, 1000);
     }
